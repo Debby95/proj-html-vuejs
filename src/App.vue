28e21c850fd3b4d2eea91e96a4f4bd93a148838b
Debby95 <debbyjacksonstar@hotmail.it>
2022-07-01 11:44:12 +0200

    install librerie + import

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,13 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/scss/main.scss";
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
